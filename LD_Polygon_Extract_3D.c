5785f84f5d1edd3490d94067323ad87b0eb82143
Luis Delgado <44483231+Luis-D@users.noreply.github.com>
2018-10-29 01:50:16 -0400

    Add files via upload

---
+++ LD_Polygon_Extract_3D.c
@@ -0,0 +1,152 @@
+#include "LD_Polygon_Extract_3D.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+
+/*****************************************************/
+/*  DEFS
+/*****************************************************/
+
+Edge_2D_Struct * Triangles_3D_Extract_Border_Edges_to_2D_no_Z
+(float * Compatible_RAW_3D_Triangles_Buffer,
+int Number_of_Triangles_in_Buffer,
+int * Edges_Count_Return) 
+{
+    if(Number_of_Triangles_in_Buffer<=0){*Edges_Count_Return=0;return NULL;}
+
+    Edge_2D_Linked_List_Struct * LinkedList = 
+    (Edge_2D_Linked_List_Struct*) malloc(sizeof(Edge_2D_Linked_List_Struct));
+
+    Edge_2D_Linked_List_Struct * LinkedListCurrent = LinkedList;
+    Edge_2D_Linked_List_Struct * LinkedListExplorer;
+    Edge_2D_Linked_List_Struct * LinkedListNext;
+    Edge_2D_Linked_List_Struct * LinkedListPast;
+
+    int EC=0;
+
+    Triangle_3D_struct * TriangleBuffer = 
+    (Triangle_3D_struct*)Compatible_RAW_3D_Triangles_Buffer;
+    Triangle_3D_struct * TriangleBuffer_END = (TriangleBuffer+Number_of_Triangles_in_Buffer);
+
+    Edge_3D_Struct tmpEdge3D;
+    Edge_2D_Struct * exp;
+
+    Vertex_3D_Struct * vert;
+    Vertex_3D_Struct * vert_exp;
+    Vertex_3D_Struct * vert_LAST;
+
+    for(;TriangleBuffer<TriangleBuffer_END;TriangleBuffer++)
+    {
+        vert = &TriangleBuffer->Point_A;
+        vert_exp =vert;
+        vert_LAST = &TriangleBuffer->Point_C;
+        for(;vert_exp<=vert_LAST;vert_exp++)
+        {
+
+            memcpy(&tmpEdge3D.Point_A,vert_exp,sizeof(Vertex_3D_Struct));
+            if(vert_exp==vert_LAST)
+            {memcpy(&tmpEdge3D.Point_B,vert,sizeof(Vertex_3D_Struct));}
+            else{memcpy(&tmpEdge3D.Point_B,vert_exp+1,sizeof(Vertex_3D_Struct));}
+
+             
+            char boolean=0;
+
+            if(EC>0)
+            {         
+                
+                LinkedListExplorer = LinkedList;  
+
+                while(LinkedListExplorer!=NULL)
+                {
+                    exp = &LinkedListExplorer->Data;
+                    LinkedListNext = LinkedListExplorer->Next;
+
+                    char bool1=    exp->Point_A[0] == tmpEdge3D.Point_A[0] && 
+                        exp->Point_A[1] == tmpEdge3D.Point_A[1] &&
+                        exp->Point_B[0] == tmpEdge3D.Point_B[0] && 
+                        exp->Point_B[1] == tmpEdge3D.Point_B[1];
+
+                    char bool2 =    
+                        exp->Point_B[0] == tmpEdge3D.Point_A[0] && 
+                        exp->Point_B[1] == tmpEdge3D.Point_A[1] &&
+                        exp->Point_A[0] == tmpEdge3D.Point_B[0] && 
+                        exp->Point_A[1] == tmpEdge3D.Point_B[1]; 
+
+                    if(bool1 || bool2)
+                    {
+                        if(LinkedListExplorer==LinkedList)
+                        {
+                            LinkedList=LinkedListNext;
+                            
+                        }
+                        else
+                        {
+                            LinkedListPast->Next=LinkedListNext;
+                           
+                        }
+
+                        free(LinkedListExplorer);
+                    EC--;
+                    boolean =1;
+                    break;
+                    }
+
+                    LinkedListPast=LinkedListExplorer;
+                    LinkedListExplorer=LinkedListNext;
+
+                    
+                }
+            }
+            if(boolean==0)
+            {
+                LinkedListCurrent->Next = (Edge_2D_Linked_List_Struct*)
+                malloc(sizeof(Edge_2D_Linked_List_Struct));
+
+                LinkedListExplorer = LinkedListCurrent->Next;
+
+                exp = &LinkedListCurrent->Data;
+
+                memcpy(exp->Point_A,&tmpEdge3D.Point_A,4*2);
+                memcpy(exp->Point_B,&tmpEdge3D.Point_B,4*2);
+
+                LinkedListCurrent=LinkedListExplorer;      
+                LinkedListCurrent->Next=NULL;
+                EC++;  
+            }
+        }
+    }
+
+
+    LinkedListExplorer = LinkedList;  
+
+
+    Edge_2D_Struct * Return = (Edge_2D_Struct*) malloc
+    (sizeof(Edge_2D_Struct) * EC);
+
+    LinkedListCurrent=LinkedList;
+
+    Edge_2D_Struct * tmp;
+
+    for(exp=Return;exp<(Return+EC);exp++)
+    {
+        tmp = &LinkedListCurrent->Data;     
+
+        memcpy(exp->Point_A,tmp->Point_A,4*2);
+        memcpy(exp->Point_B,tmp->Point_B,4*2);
+
+        LinkedListCurrent=LinkedListCurrent->Next;
+    }
+
+    LinkedListCurrent=LinkedList;
+
+    while(LinkedListCurrent!=NULL)
+    {
+        LinkedListExplorer = LinkedListCurrent->Next;
+        free(LinkedListCurrent);
+        LinkedListCurrent=LinkedListExplorer;
+    }
+
+    *Edges_Count_Return = EC;
+    return Return;
+}
---
+++ LD_Polygon_Extract_3D.h
@@ -0,0 +1,80 @@
+/***********************************************************************
+MIT License
+
+Copyright (c) 2018 Luis Delgado
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+****************************************************************************/
+
+/*
+	This is a collection of algorithms to extract data (like submeshes) from 3D meshes
+*/
+
+#ifndef _LD_POLYGON_EXTRACT_3D_H
+#define _LD_POLYGON_EXTRACT_3D_H
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+}Vertex_3D_Struct;
+
+typedef struct
+{
+    float Point_A[3];
+    float Point_B[3];
+}Edge_3D_Struct;
+
+typedef struct
+{
+    float Point_A[2];
+    float Point_B[2];
+}Edge_2D_Struct;
+
+typedef struct
+{
+    Vertex_3D_Struct Point_A;
+    Vertex_3D_Struct Point_B;
+    Vertex_3D_Struct Point_C;
+}Triangle_3D_struct;
+
+typedef struct Edge_2D_Linked_List_Struct
+{
+    Edge_2D_Struct Data;
+    struct Edge_2D_Linked_List_Struct * Next;
+}Edge_2D_Linked_List_Struct;
+
+
+
+/*****************************************************/
+/*  DECL
+/*****************************************************/
+
+/***************************************************
+	Given a mesh made out of triangles with three components,
+	This algorithm extracts only the border edges.
+	This algorithm ignores the Z component.
+	The result is a 2D Polygon.
+***************************************************/
+Edge_2D_Struct * Triangles_3D_Extract_Border_Edges_to_2D_no_Z
+(float * Compatible_RAW_3D_Triangles_Buffer,int Number_of_Triangles_in_Buffer,
+int * Edges_Count_Return);
+
+#endif
